c87a9dde0c880a0cefd8d5c54c29063c85326348
Souhail El fchtali <sel-fcht@e0r2p5.1337.ma>
2021-09-26 16:07:34 +0100

    test

--- philo.c
+++ philo.c
@@ -6,8 +6,23 @@
 /*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 13:10:20 by sel-fcht          #+#    #+#             */
-/*   Updated: 2021/09/26 13:10:35 by sel-fcht         ###   ########.fr       */
+/*   Updated: 2021/09/26 16:06:51 by sel-fcht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philosophers.h"
+//#include "philosophers.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+   while(1)
+   {
+       printf("fist philo is thinking \n");
+       sleep(1);
+       printf("philo is sleeping\n");
+       sleep(1);
+       printf("philo is eating \n");
+       sleep(1);
+   }
+}
