4a236338f1fdb64fb8aa1d4a484c59a34ed2f44c
Souhail El fchtali <sel-fcht@e0r5p5.1337.ma>
2021-09-27 15:20:04 +0100

    hh

--- philo.c
+++ philo.c
@@ -6,16 +6,24 @@
 /*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 13:10:20 by sel-fcht          #+#    #+#             */
-/*   Updated: 2021/09/26 16:08:49 by sel-fcht         ###   ########.fr       */
+/*   Updated: 2021/09/27 15:19:33 by sel-fcht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "philosophers.h"
-#include<stdio.h>
-#include<stdlib.h>
 #include<unistd.h>
+#include <stdlib.h>
+ 
 int main()
 {
+    //TODO:
+    // mutex and pthreads
+    //forks process 
+    // deadlock concept
+    // each philo is a thread
+    //protect forks with a mutex to avoid duplicating forks
+    //every philo is a process | fork ->shared resource 
+    
    while(1)
    {
        printf("fist philo is thinking \n");
--- philo.h
+++ philo.h
@@ -6,12 +6,12 @@
 /*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 13:10:37 by sel-fcht          #+#    #+#             */
-/*   Updated: 2021/09/26 13:11:24 by sel-fcht         ###   ########.fr       */
+/*   Updated: 2021/09/26 16:12:46 by sel-fcht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHER_H
-#define PHILOSOPHER_H
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
 
 #include <unistd.h>
 #include <math.h>
